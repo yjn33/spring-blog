f43c10caeaaca42734b2b819ed4418752feec7ac
yjn33 <yjn0032@gmail.com>
2025-05-03 22:45:51 +0900

    feat : Aritcle 도메인 엔터티 생성

---
+++ src/main/java/com/example/blog/domain/article/entity/Article.java
@@ -0,0 +1,41 @@
+package com.example.blog.domain.article.entity;
+
+
+import com.example.blog.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@Table(name = "article")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증감
+	@Column(name = "id", updatable = false)
+	private Long id;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "content", nullable = false)
+	private String content;
+
+
+	@Builder
+	public Article(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+}
