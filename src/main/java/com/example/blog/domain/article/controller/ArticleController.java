7058e32914b4e9707beee860e5726999663d30e5
yjn33 <yjn0032@gmail.com>
2025-05-19 15:23:54 +0900

    feat : 게시물 저장 컨트롤러 구현

--- src/main/java/com/example/blog/domain/article/controller/ArticleController.java
+++ src/main/java/com/example/blog/domain/article/controller/ArticleController.java
@@ -1,11 +1,37 @@
 package com.example.blog.domain.article.controller;
 
 
+import com.example.blog.domain.article.dto.request.ArticleRequestDto;
+import com.example.blog.domain.article.dto.response.ArticleResponseDto;
+import com.example.blog.domain.article.entity.Article;
+import com.example.blog.domain.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("api")
+@RestController
+@RequestMapping("api")
 public class ArticleController {
+
+	private final ArticleService articleService;
+
+	/*
+		게시물 저장
+	*/
+	@PostMapping("/v1/articles")
+	public ResponseEntity<Article> saveArticle(@RequestBody ArticleRequestDto articleRequestDto) {
+		Article savedArticle = articleService.saveArticle(articleRequestDto);
+
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(savedArticle);
+	}
+
+
+
 
 }
