4221c6b3bdc3235bddfd39a456d7f295a65855bf
yjn33 <yjn0032@gmail.com>
2025-05-19 12:50:21 +0900

    feat : ArticleService 글 추가 메서드 구현

--- src/main/java/com/example/blog/domain/article/dto/request/ArticleRequestDto.java
+++ src/main/java/com/example/blog/domain/article/dto/request/ArticleRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.blog.domain.article.dto.request;
 
 
+import com.example.blog.domain.article.entity.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,16 @@ public class ArticleRequestDto {
 	private String title;
 
 	private String content;
+
+
+	// 요청 데이터를 엔터티로 생성
+	// static factory method와 다르다 -> 이미 요청받은 엔터티를 생성하기 위해서
+	public Article toEntity() {
+		return Article.builder()
+			.title(this.title)
+			.content(this.content)
+			.build();
+	}
 
 
 }
--- src/main/java/com/example/blog/domain/article/service/ArticleService.java
+++ src/main/java/com/example/blog/domain/article/service/ArticleService.java
@@ -3,6 +3,7 @@ package com.example.blog.domain.article.service;
 
 import com.example.blog.domain.article.dto.request.ArticleRequestDto;
 import com.example.blog.domain.article.dto.response.ArticleResponseDto;
+import com.example.blog.domain.article.entity.Article;
 import com.example.blog.domain.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +18,8 @@ public class ArticleService {
 	/*
 		글 추가 메서드
 	*/
-	public ArticleResponseDto saveArticle(ArticleRequestDto articleRequestDto) {
-		return articleRepository.save
+	public Article saveArticle(ArticleRequestDto articleRequestDto) {
+		return articleRepository.save(articleRequestDto.toEntity());
 	}
 
 
