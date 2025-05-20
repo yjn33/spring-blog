0cd5f999beb52a0d9b4de433b68b0f42cb9cc0d2
yjn33 <yjn0032@gmail.com>
2025-05-20 12:50:51 +0900

    feat : BaseEntityResponseDto 상속 설정

--- src/main/java/com/example/blog/domain/article/dto/response/ArticleResponseDto.java
+++ src/main/java/com/example/blog/domain/article/dto/response/ArticleResponseDto.java
@@ -2,13 +2,14 @@ package com.example.blog.domain.article.dto.response;
 
 
 import com.example.blog.domain.article.entity.Article;
+import com.example.blog.global.common.base.dto.BaseEntityResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
 @AllArgsConstructor
 @Getter
-public class ArticleResponseDto {
+public class ArticleResponseDto extends BaseEntityResponseDto {
 
 	private final Long id;
 
@@ -17,6 +18,8 @@ public class ArticleResponseDto {
 	private final String content;
 
 	public ArticleResponseDto(Article article) {
+		super.createAt = article.getCreateAt();
+		super.modifiedAt = article.getModifiedAt();
 		this.id = article.getId();
 		this.title = article.getTitle();
 		this.content = article.getContent();
